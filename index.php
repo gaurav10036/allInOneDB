14cea12a41c64dd65d949e3682a2cafebb763b2b
Gaurav Sirauthiya <gaurav10036@users.noreply.github.com>
2016-03-28 01:47:06 +0530

    Added files via upload
    index file

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+require_once('lib/config.php');
+require_once('lib/autoload.php');
+/**
+* @author: Gaurav Sirauthiya
+* @version: 1.0.0
+*/
+
+$template->getHeader();
+$template->getFooter();
+?>
